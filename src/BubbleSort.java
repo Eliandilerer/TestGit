fc8e97e33c4328143b9ea1b26890410e96a1b4d5
Eliandilerer <illya.zp1992@mail.ru>
2020-02-28 14:53:05 +0200

    Second commit with bubbleSort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,6 +2,50 @@ public class BubbleSort {
 
     public static void main (String[] args){
 
-        System.out.println("Hello World!");
+        int[] arr = new int[10];
+        for(int i = 0; i < arr.length; i++)
+        {
+            arr[i] = (int)(Math.random() * 100);
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        bubbleSort(arr);
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+    }
+
+    public static void bubbleSort(int[] arr)
+    {
+        int flag = 0;
+
+        for(int i = 0; i < arr.length; i++)
+        {
+            for(int j = 0; j < arr.length - i - 1; j++)
+            {
+                if(arr[j] > arr[j + 1])
+                {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                    flag = 1;
+                }
+            }
+            if(flag == 1)
+            {
+                flag = 0;
+            }
+            else
+            {
+                break;
+            }
+        }
     }
 }
